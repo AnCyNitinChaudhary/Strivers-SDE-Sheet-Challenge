71af7bd16bb6f367861affb48f75d3bccd90af84
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-06-05 01:36:50 +0530

    Create UniquePaths.cpp

---
+++ Day2/UniquePaths.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h> 
+// int f(int i,int j){
+// 	if(i==0&&j==0)return 1;
+// 	if(i<0||j<0)return 0;
+// 	int up=f(i-1,j);
+// 	int left=f(i,j-1);
+// 	return up+left;
+// }
+// int f1(int i,int j,vector<vector<int>>&dp){
+// 	if(i==0&&j==0)return 1;
+// 	if(i<0||j<0)return 0;
+// 	if(dp[i][j]!=-1)return dp[i][j];
+// 	int up=f1(i-1,j,dp);
+// 	int left=f1(i,j-1,dp);
+// 	return dp[i][j]=up+left;
+// }
+int uniquePaths(int m, int n) {
+	// Write your code here.
+	//I am going to solve this question by all the appraoches of DP
+	//Recursive
+	// return f(m-1,n-1);
+	//Memoization
+	// vector<vector<int>>dp(m,vector<int>(n,-1));
+	// return f1(m-1,n-1,dp);
+	//Tabulation
+	// vector<vector<int>>dp(m,vector<int>(n,0));
+	// for(int i=0;i<m;i++){
+	// 	for(int j=0;j<n;j++){
+	// 		if(i==0&&j==0)dp[i][j]=1;
+	// 		else{
+	// 			int up=0;
+	// 			int left=0;
+	// 			if(i>0)up+=dp[i-1][j];
+	// 			if(j>0)left+=dp[i][j-1];
+	// 			dp[i][j]=up+left;
+	// 		}
+	// 	}
+	// }
+	// return dp[m-1][n-1];
+
+	//Space optimization-
+	vector<int>prev(n,0);
+	for(int i=0;i<m;i++){
+		vector<int>temp(n,0);
+		for(int j=0;j<n;j++){
+			if(i==0&&j==0)temp[j]=1;
+			else{
+				int up=0;
+				int left=0;
+				if(i>0)up+=prev[j];
+				if(j>0)left+=temp[j-1];
+				temp[j]=up+left;
+			}
+		}
+		prev=temp;
+	}
+	return prev[n-1];
+}
