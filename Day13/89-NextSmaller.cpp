4b34d96d081f89a4a93c35841e3427ad4a85c694
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-07-27 08:41:30 +0530

    Create 89-NextSmaller.cpp

---
+++ Day13/89-NextSmaller.cpp
@@ -0,0 +1,37 @@
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    vector<int>ans(n);
+    // ans[n-1]=-1;
+    // // priority_queue<int>pq;
+    // // pq.push(arr[n-1]);
+    // for(int i=n-2;i>=0;i--){
+    //     int mini=-1;
+    // for(int j=i+1;j<n;j++){
+    //     if(arr[j]<arr[i]){
+    //         mini=arr[j];
+    //         break;
+    //     }
+    // }
+    // ans[i]=mini;
+    // }
+    // return ans;
+
+
+    stack<int> s;
+    for (int i = n - 1; i >= 0; i--) {
+        while (!s.empty() && s.top() >= arr[i]) {
+            s.pop();
+        }
+        if (s.empty()) {
+            ans[i] = -1;
+        } 
+        else {
+            ans[i] = s.top();
+        }
+
+        s.push(arr[i]);
+    }
+    return ans;
+}
